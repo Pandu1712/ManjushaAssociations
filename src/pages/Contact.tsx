b781a8a7603a6e07d8cbafd83989e6df0688a232
Pandu1712 <prasannakumarpanda2000@gmail.com>
2026-01-06 19:12:56 +0530

    Google Map

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -196,7 +196,7 @@ const Contact = () => {
             <div className="bg-white rounded-2xl shadow-2xl overflow-hidden">
               <div className="h-96">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243.7076!2d81.4574!3d16.5167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTbCsDMxJzAwLjEiTiA4McKwMjcnMjYuNiJF!5e0!3m2!1sen!2sin!4v1234567890"
+                  src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d432.95763450593586!2d81.5688444!3d16.4009353!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e1!3m2!1sen!2sin!4v1767706901165!5m2!1sen!2sin"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
